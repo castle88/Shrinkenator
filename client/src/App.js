9c527ba7b4de4131354625f88d8d42dbca292e36
Castle <castlerock88@protonmail.com>
2022-03-17 04:37:22 -0400

    cleaned that bs up

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,7 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-=======
->>>>>>> d-8
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-<<<<<<< HEAD
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
+      <header>
        <h1>Welcome to Shrinkenator</h1>
        <p>Crushing your ugly long links into neat little packages</p>
         <form>
@@ -37,7 +16,6 @@ function App() {
         <h2>Preview your link:</h2>
         <p>http://www.shrinkenator.com/{/* link name here */}</p>
       </main>
->>>>>>> d-8
     </div>
   );
 }
