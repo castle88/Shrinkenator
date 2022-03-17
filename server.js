af33abfde536d0170e5414eecfd82de2b4f6a9cc
Castle <castlerock88@protonmail.com>
2022-03-17 01:39:45 -0400

    home and link routes

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express')
 require('dotenv').config({ path: './config/.env' })
 const port = process.env.PORT || 2121
 const app = express()
+const homeRoutes = require('./routes/homeRoutes')
+const linkRoutes = require('./routes/linkRoutes')
 
 app.listen(port, () => console.log(`Server running port: ${port}`))
